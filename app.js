be474a070b2e56657458063f28dba96326fdbbe0
islam <islamabdullaev33@gmail.com>
2025-08-30 19:20:45 +0300

    I designed the title of page with HTML-CSS

---
+++ app.js
@@ -0,0 +1,14 @@
+// 1- Design the HTML page
+// 2- Add styles
+
+
+// 3- Use an array which contains "rock", "paper", and "scissors"
+// 4- Randomly pick an option from the array above
+// 5- Generate an image related to randomly picked option (rock, paper or scissors)
+// 6- Generate all 3 images in a div for 5 seconds so the player can have a better game experience
+// 7- Program the logic (if player is rock and computer is paper computer wins etc.)
+// 8- Create a paragraph element on DOM which contains "player win" or "computer win"
+
+
+
+// Maybe I can program a "betting" logic later..
